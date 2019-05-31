610230b232799d6de7d38cf782d21e78cebd6a2b
jovanimtzrico <jovanimtzrico@gmail.com>
2019-05-31 16:01:55 -0700

    String Permutations

--- Recursion/string_permutations.py
+++ Recursion/string_permutations.py
@@ -10,6 +10,28 @@
 # string = 'abc'
 # output = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
 
+# Solution
+def permutations1(string):
+    return return_permutations(string, 0)
+    
+def return_permutations(string, index):
+    # Base Case
+    if index >= len(string):
+        return [""]
+    
+    small_output = return_permutations(string, index + 1)
+    
+    output = list()
+    current_char = string[index]
+    
+    # iterate over each permutation string received thus far
+    # and place the current character at between different indices of the string
+    for permutation in small_output:
+        for index in range(len(small_output[0]) + 1):
+            new_permutation = permutation[0: index] + current_char + permutation[index:]
+            output.append(new_permutation)
+    return output
+
 
 def permutations(string):
     """
